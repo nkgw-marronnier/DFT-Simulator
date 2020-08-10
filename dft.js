a9a0cc9f8ea52f70851b1627e0a399606c2f59ee
nkgw <s1832099lh@s.chibakoudai.jp>
2020-08-10 15:33:59 +0900

    Update dft.js

--- dft.js
+++ dft.js
@@ -1,6 +1,12 @@
 var siki = 0;
 var sample = 0;
 var P = 0;
+var jissuu = [];
+var sum = 0;
+var fd = [];
+
+event();
+graph();
 
 function button() {
   var shiki = document.getElementsByName("siki");
@@ -15,18 +21,17 @@ function button() {
       sample = i;
     }
   }
-
   if (chart) {
     chart.destroy();
+  }
+  if (chart2) {
     chart2.destroy();
   }
-
   event();
+  graph();
 }
 
 function event() {
-
-  console.log(siki);
 
   if (sample == 0) {
     P = 10;
@@ -39,49 +44,63 @@ function event() {
   } else if (sample == 4) {
     P = 1000;
   }
-  var jissuu = [];
 
   // 原関数定義
   function func_y(x) {
     if (siki == 0) {
-      console.log(1);
       return 1 * Math.sin(2 * x);
     } else if (siki == 1) {
-      console.log(2);
       return 1 * Math.sin(8 * x);
     } else if (siki == 2) {
       return 5 * Math.sin(20 * x);
     } else if (siki == 3) {
       return 1 * Math.sin(2 * x) + 1 * Math.cos(4 * x);
     }
-    //return 1 * Math.sin(2 * x) + 7.0 * Math.cos(3.0 * x);
   }
 
   let f = new Array(P);
   // データサンプリング
   for (let m = 0; m < P; m++) {
     f[m] = func_y(((2.0 * Math.PI) / P) * m);
-    //console.log(f[m]);
   }
-  var sum = 0;
   for (let i = 0, len = f.length; i < len; i++) {
+    fd[i] = f[i];
     sum += 1;
   }
+
+  // DFT係数計算
+  for (let n = 0; n < P; n++) {
+    let ar = 0.0;
+    let ai = 0.0;
+    let x;
+    for (let m = 0; m < P; m++) {
+      x = ((2.0 * Math.PI) / P) * m * n;
+      ar += f[m] * Math.cos(-x);
+      ai += f[m] * Math.sin(-x);
+    }
+    ar /= P;
+    ai /= P;
+    x = Math.sqrt(4.0 * ar * ar + 4.0 * ai * ai);
+    jissuu[n] = Math.round(x * 100) / 100;
+  }
+}
+
+function graph() {
+
   var flabel = [...Array(sum).keys()]
 
-
-  const canvas = document.getElementById('canvas');
   const mydata = {
     labels: flabel,
 
     datasets: [{
       label: '時間領域',
-      data: f,
+      data: fd,
       borderColor: '#5f5',
       fill: false
     }],
   };
-  const chart = new Chart(canvas, {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  window.chart = new Chart(ctx, {
 
     type: 'line',
     data: mydata,
@@ -103,30 +122,7 @@ function event() {
     }
   });
 
-  // DFT係数計算
-  //console.log("次数\t実数部\t虚数部\t絶対値");
-  for (let n = 0; n < P; n++) {
-    let ar = 0.0;
-    let ai = 0.0;
-    let x;
-    for (let m = 0; m < P; m++) {
-      x = ((2.0 * Math.PI) / P) * m * n;
-      ar += f[m] * Math.cos(-x);
-      ai += f[m] * Math.sin(-x);
-    }
-    ar /= P;
-    ai /= P;
-    x = Math.sqrt(4.0 * ar * ar + 4.0 * ai * ai);
-    /*console.log(
-      n,
-      Math.round(ar * 100) / 100,
-      Math.round(ai * 100) / 100,
-      Math.round(x * 100) / 100
-    );*/
-    jissuu[n] = Math.round(x * 100) / 100;
-  }
-
-  const canvas2 = document.getElementById('canvas2');
+  var ctx2 = document.getElementById('canvas2').getContext('2d');
   const mydata2 = {
     labels: flabel,
 
@@ -136,7 +132,8 @@ function event() {
       backgroundColor: '#aaf'
     }]
   };
-  const chart2 = new Chart(canvas2, {
+
+  window.chart2 = new Chart(ctx2, {
 
     type: 'bar',
     data: mydata2,
@@ -157,7 +154,5 @@ function event() {
       }
     }
   });
-
+  sum = 0;
 }
-
-event();
