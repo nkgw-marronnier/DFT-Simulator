862ca035d84a32b21e2a8aaac9fa48e197d3639f
nkgw <s1832099lh@s.chibakoudai.jp>
2020-08-12 13:34:59 +0900

    Update dft-ae.js

--- js/dft-ae.js
+++ js/dft-ae.js
@@ -93,9 +93,10 @@ function calculate() {
   for (let m = 0; m < P; m++) {
     f[m] = func_y(((2.0 * Math.PI) / P) * m);
   }
+  // これはグラフ用であって標本化には関係ない
   for (let i = 0, len = f.length; i < len; i++) {
     fd[i] = f[i];
-    sum += 1; // これは標本化には関係ない
+    sum += 1;
   }
 
   // DFT係数計算
